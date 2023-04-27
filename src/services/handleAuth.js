ad6c1658706fd70796fb894531c4d4e6d61846bd
Vahe-1810 <vahevardanyan2000@gmail.com>
2023-04-27 17:07:18 +0400

    some sittle changes

--- src/services/handleAuth.js
+++ src/services/handleAuth.js
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth, db } from "../firebase";
+import { auth } from "../firebase";
 import { setUser } from "../store/slicers/userSlice";
 import { getUserDB, setUserDB } from "./dataBaseConfig";
 import { useAuth } from "../hooks/useAuth";
@@ -17,11 +17,13 @@ export const emailSignUp = async (email, password, phone, setLoading) => {
     .finally(() => setLoading(false));
 };
 
-export const emailSignIn = async (email, password) => {
+export const emailSignIn = async (email, password, setLoading) => {
   try {
     return await signInWithEmailAndPassword(auth, email, password);
   } catch ({ message }) {
     return console.log(message);
+  } finally {
+    setLoading(false);
   }
 };
 
@@ -30,19 +32,26 @@ export const useAuthListener = (setLoading) => {
   const disp = useDispatch();
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      const dbData = await getUserDB(user.uid);
-      disp(
-        setUser({
-          userInfo: {
-            ...userInfo,
-            ...dbData,
-            photoURL: user?.photoURL,
-          },
-          email: user?.email,
-          token: user?.accessToken,
-          id: user?.uid,
-        })
-      );
+      try {
+        const dbData = user && (await getUserDB(user.uid));
+
+        disp(
+          setUser({
+            userInfo: user
+              ? {
+                  ...userInfo,
+                  ...dbData,
+                  photoURL: user.photoURL,
+                }
+              : {},
+            email: user?.email,
+            token: user?.accessToken,
+            id: user?.uid,
+          })
+        );
+      } catch (e) {
+        console.log(e);
+      }
       setLoading(false);
     });
     return () => unsubscribe();
