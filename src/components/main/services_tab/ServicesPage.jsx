a0a3ed21e72997065077d1c23a2d3a69e48b0747
-dav <davtyan.arman1996@gmail.com>
2023-05-04 17:15:52 +0400

    my Changes

--- src/components/main/services_tab/ServicesPage.jsx
+++ src/components/main/services_tab/ServicesPage.jsx
@@ -5,9 +5,8 @@ import {
   ListSubheader,
   Typography,
 } from "@mui/material";
-import React, { useEffect } from "react";
 import { SERVICE_DATA } from "./booking_form/servicesData";
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react";
 // import ResponsiveGrid from "../services_tab/GridTabServices";
 // import { Booking } from "../services_tab/Booking";
 
@@ -49,41 +48,7 @@ const ServicesPage = ({ setActiveLinkId }) => {
 export default ServicesPage;
 
 {
-  /* <Box sx={{ flexGrow: 1, mt: 10, ml: 20, mr: 20 }}>
-        {Array(Math.ceil(servicesData.length / 3))
-          .fill()
-          .map((_, i) => (
-            <Grid container spacing={8} key={i}>
-              {servicesData.slice(i * 3, i * 3 + 3).map((sv) => (
-                <Grid item xs={12} sm={6} md={4} key={sv.name}>
-                  <Link to={sv.name}>
-                    <Item
-                      src={sv.url}
-                      sx={{
-                        // flexGrow: 1,
-                        mt: 8,
-                        ml: 2,
-                        mr: 2,
-                      }}
-                    >
-                      <SelectedService sv={sv} />
-                    </Item>
-                  </Link>
-                </Grid>
-              ))}
-            </Grid>
-          ))}
-      </Box> */
-  // const onClose = () => {
-  //   setService(!service);
-  // };
-  // const Item = styled(Paper)(({ theme }) => ({
-  //   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  //   ...theme.typography.body2,
-  //   padding: theme.spacing(12),
-  //   textAlign: "center",
-  //   color: theme.palette.text.secondary,
-  //   transition: "transform 0.3s ease-in-out",
+  
   //   "&:hover": {
   //     transform: "translateY(-40px)",
   //   },
