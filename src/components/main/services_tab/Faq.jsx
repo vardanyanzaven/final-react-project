73a13811caa2d28d08b625ef5d388438ffbc9b00
terezshashikyan <78693438+terezshashikyan@users.noreply.github.com>
2023-05-29 16:36:59 +0400

    final changes

--- src/components/main/services_tab/Faq.jsx
+++ src/components/main/services_tab/Faq.jsx
@@ -1,43 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import "./Faq.css"
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
-
-
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../../firebase';
 
 const FAQ = () => {
-//   const [faqs, setFaqs] = useState([]);
+const [faqs, setFaqs] = useState([]);
 
-  const faqs = [
-    {
-        question: "What types of cars do you offer?",
-        answer: "We offer a wide range of vehicles from economy to luxury depending on your needs. Our fleet includes compact cars, sedans, SUVs, vans, and more.",
-    },
-    {
-        question: "What documents do I need to rent a car?",
-        answer: "You will need a valid driver's license, a credit card in your name, and proof of insurance. If you're renting internationally, you may also need an international driving permit and a passport.",
-    },
-    {
-        question: "Can I add extra drivers to my rental?",
-        answer: "Yes, you can add additional drivers to your rental for an extra fee. They must meet the same requirements as the primary driver and be included in the rental agreement.",
-    },
-    {
-        question: "What happens if I return the car late?",
-        answer: "Late returns may result in additional fees. Please contact us as soon as possible if you anticipate being late to avoid penalties.",
-    }
-  ]
+  
 
-//   useEffect(() => {
-//     const getFaqs = async () => {
-//       const db = firebase.firestore();
-//       const faqCollection = db.collection('faqs');
-//       const faqSnapshot = await faqCollection.get();
-//       const faqData = faqSnapshot.docs.map((doc) => doc.data());
-//       setFaqs(faqData);
-//     };
+  useEffect(() => {
+    const getFaqs = async () => {
+      const ref = doc(db, "faqs", "v2uD6ABcDBZqM0cAteGk");
+      const snapShot = await getDoc(ref);
+      setFaqs(snapShot.data().faqs);
+    };
+  
 
-//     getFaqs();
-//   }, []);
+    getFaqs();
+  }, []);
 
   return (
     <div className = "faq_container">
